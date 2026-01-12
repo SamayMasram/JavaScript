921acc82cc1aa33d86391f0ae883ce80d786e254
Samay Masram <samaymasram1404@gmail.com>
2026-01-12 01:25:52 +0000

    variables

---
+++ Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553 
+let accountEmail = "samay@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState; //undefined
+
+//accountId = 2     not allowed
+accountEmail = "s@gmail.com"
+accountPassword = "98765"
+accountCity = "Mumbai"
+
+/*
+    Prefer not to use var 
+    because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
