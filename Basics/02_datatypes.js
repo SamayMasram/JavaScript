d3bf2c1ff81c5a154ab07e91ee59b38d233c7e16
Samay Masram <samaymasram1404@gmail.com>
2026-01-13 02:13:31 +0000

    datatypes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) //we are using ndoe js not browser
+//code readability should be high
+
+let name = "samay"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+/* 
+    number -> 2^53
+    bigint
+    string -> ""
+    boolean -> true/false
+    null -> standalone
+    undefined
+    symbol -> unique
+    object
+*/
+
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
