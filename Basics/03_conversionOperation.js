5b8ccf385b3eac1325ce0dd182cfc03b1a20e619
Samay Masram <samaymasram1404@gmail.com>
2026-01-16 02:15:46 +0000

    conversion operation

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -31,3 +31,31 @@ let someNumber = 33
 let stringNumber = String (someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+//---------------OPERATIONS------------------
+ 
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((3+4)*5%3)
+
+console.log(true);
+console.log(+true);
+
