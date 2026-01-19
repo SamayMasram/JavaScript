a27dcf0980dfb5dd794ff891b69f4f3d0a1f3303
Samay Masram <samaymasram1404@gmail.com>
2026-01-19 16:21:33 +0000

    Arrays

--- Intermediate/01_array.js
+++ Intermediate/01_array.js
@@ -30,3 +30,27 @@ const myn2 = myArr.splice(1, 3)
 console.log("C", myArr);
 console.log(myn2);
 // splice modify original array
+
+const marvel_heros = ["thor", "ironman", "hulk"]
+const dc_heros = ["superman", "batman", "flash"]
+//marvel_heros.push(dc_heros);
+console.log(marvel_heros);
+//console.log(marvel_heros[3][1]);
+
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
+
+const allNewHeros = [...marvel_heros, ...dc_heros]
+console.log(allNewHeros);
+
+const another_array = [1,2,3,[4,5,6,],7,[6,7,[4,5]]]
+const real_another_array = another_array.flat(Infinity) //give depth
+console.log(real_another_array);
+
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh")); //convert to array
+console.log(Array.from({name: "hitesh"})); // Interesting
+
+let score1 = 100
+let score2 = 200
+console.log(Array.of(score1, score2));
